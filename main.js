abc44cbdb14571e43cdd415b353d5e07084971b3
Salman778 <alk-333@hotmail.com>
2019-11-26 21:01:58 +0300

    warmup of js advanced

---
+++ main.js
@@ -0,0 +1,51 @@
+// 1 Squares
+const nums = [1, 2, 3, 4, 5];
+let numsSquared = [];
+
+// Use .map() to get the square of nums and store the value in numsSquared
+// numsSquared => [1, 4, 9, 16, 25]
+
+
+
+
+
+// 2 isDivisibleBy3
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let isDivisibleBy3 = [];
+
+// Use .filter() to find out the numbers that are divisible by 3 and store the value in isDivisibleBy3
+// isDivisibleBy3 => [3, 6, 9]
+
+
+
+
+
+// 3 Abbreviations
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let dayAbbreviations = [];
+
+// Find the abbreviation of all days and add them to dayAbbreviations array
+// dayAbbreviations => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+
+
+
+
+// 4 Capitalize
+const instructors = ['ahmed', 'maha', 'sami', 'salman'];
+let capitalizedInstructors = [];
+
+// Capitalize all the strings in the instructors array and store them in the array capitalizedInstructors.
+// capitalizedInstructors => ["AHMED", "MAHA", "SAMI", "SALMAN"]
+
+
+
+
+
+// 5 Crazy numbers, again!
+// These crazy numbers are now strings 😯 😯  !!  
+const stringNumbers = ["103440", "3799.2663", "3.14159265359", "859494", "59439"];
+let totalNumbersUnder4000 = 0;
+
+// Convert numbers from strings to numbers and sum all numbers under 4000 and store them in totalNumbersUnder4000
+// totalNumbersUnder4000 => 3802.4078926536
